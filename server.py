c2ee325c6ff4d10b77edccce1e547b0794b0c0a7
minoj <136566821+minoj-next@users.noreply.github.com>
2023-10-04 10:20:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
 import socket
 from cryptography.fernet import Fernet
 
-# Replace '0.0.0.0' with the appropriate server address
-server_address = ('0.0.0.0', 12345)
-cipher_suite = None  # Users will provide their own encryption key
+# Generate a secret key for encryption (server-side)
+key = Fernet.generate_key()
+print("Generated Key:", key.decode())  # Print the key
+cipher_suite = Fernet(key)
 
+# Create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the server address and port
+server_address = ('0.0.0.0', 12345)  # Replace '0.0.0.0' with the your system IP
+# Replace 12345 with the same port used on the client side
+
+# Bind the socket to the server address and port
 server_socket.bind(server_address)
+
+# Listen for incoming connections
 server_socket.listen(1)
 
 print("Waiting for a connection...")
@@ -19,22 +29,22 @@ try:
         # Receive the encrypted message from the client
         encrypted_message = connection.recv(1024)
 
-        # Decrypt the message if a key is provided
-        if cipher_suite:
-            decrypted_message = cipher_suite.decrypt(encrypted_message)
-            print("Received:", decrypted_message.decode())
+        # Decrypt the message
+        decrypted_message = cipher_suite.decrypt(encrypted_message)
+
+        # Print the received message
+        print("Received:", decrypted_message.decode())
 
         # Prompt the server to enter a message
         server_input = input("Server: Enter a message (or 'quit' to exit): ")
-
+        
         # Check if the server wants to quit
         if server_input.lower() == 'quit':
             break
 
         # Encrypt and send the server's message to the client
-        if cipher_suite:
-            encrypted_response = cipher_suite.encrypt(server_input.encode())
-            connection.send(encrypted_response)
+        encrypted_response = cipher_suite.encrypt(server_input.encode())
+        connection.send(encrypted_response)
 
 finally:
     connection.close()
